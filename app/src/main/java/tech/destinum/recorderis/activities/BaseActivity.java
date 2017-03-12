d83ce3dbed4e8b64e023378aaa23a3fbe1e9a6fc
Diego Fernando Urrea <diegoug@gmail.com>
2017-03-12 17:33:48 -0500

    Big refactor

--- app/src/main/java/tech/destinum/recorderis/activities/BaseActivity.java
+++ app/src/main/java/tech/destinum/recorderis/activities/BaseActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -24,49 +25,35 @@ public class BaseActivity extends ActionBarActivity {
     public NavigationView mNavigationView;
     protected ActionBarDrawerToggle mToggle;
     protected Toolbar mToolbar;
-    //Navigation Drawer
     public DrawerLayout mDrawerLayout;
-
-
-    //App's Toolbar
-    protected Toolbar mActionBarToolbar;
 
     protected void onCreateDrawer() {
         //Instantiate Navigation Drawer
         setupNavDrawer();
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.logout) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
-
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-
-    }
-
-    //Override this method in subclasses if you want a different toolbar for the activity
-    protected Toolbar getActionBarToolbar() {
-        if (mToolbar == null) {
-            mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
-            if (mToolbar != null) {
-                setSupportActionBar(mToolbar);
-            }
-        }
-        return mToolbar;
     }
 
 
@@ -75,12 +62,10 @@ public class BaseActivity extends ActionBarActivity {
 
         //Instantiate Navigation Drawer
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
         //Instatiate Navigation Drawer List
         mNavigationView= (NavigationView) findViewById(R.id.navigation_view);
-
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawerLayout.setDrawerListener(mToggle);
         mToggle.syncState();
--- app/src/main/java/tech/destinum/recorderis/activities/PrivacyPolicy.java
+++ app/src/main/java/tech/destinum/recorderis/activities/PrivacyPolicy.java
@@ -16,39 +16,6 @@ public class PrivacyPolicy extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
         super.onCreateDrawer();
-        //Action bar needs to be instatiate in every Activity that has layout
-        setUpActionBar();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.logout) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    //Set up Action Bar
-    private void setUpActionBar() {
-        //Uses superclass method to instantiate ActionBar
-        mActionBarToolbar = getActionBarToolbar();
-        mActionBarToolbar.setTitle(getResources().getString(R.string.app_name));
-        setSupportActionBar(mActionBarToolbar);
-
-
-    }
 }
