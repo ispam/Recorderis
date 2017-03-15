0fa99af8d1230ff29939a50dbd9aef5bfb0f0186
Diego Fernando Urrea <diegoug@gmail.com>
2017-03-15 17:57:50 -0500

    Privacy Policy completed for 3 dif languages

--- app/src/main/java/tech/destinum/recorderis/activities/Policy.java
+++ app/src/main/java/tech/destinum/recorderis/activities/Policy.java
@@ -1,18 +1,29 @@
 package tech.destinum.recorderis.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
+import android.text.Html;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 import tech.destinum.recorderis.R;
 
 public class Policy extends BaseActivity {
+
+    private TextView mPrivacyPolicy;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_policy);
         super.onCreateDrawer();
+
+        mPrivacyPolicy = (TextView) findViewById(R.id.privacy_policy_text);
+
+        mPrivacyPolicy.setText(Html.fromHtml(getString(R.string.privacy_policy_text)));
 
     }
 }
