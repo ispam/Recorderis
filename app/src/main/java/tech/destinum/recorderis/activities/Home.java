2633d5ab522e7da50c96bcde126516b19ea5717a
Diego Fernando Urrea <diegoug@gmail.com>
2017-06-25 21:46:12 -0500

    DB Logic changed

--- app/src/main/java/tech/destinum/recorderis/activities/Home.java
+++ app/src/main/java/tech/destinum/recorderis/activities/Home.java
@@ -39,39 +39,6 @@ public class Home extends BaseActivity {
         mRecyclerViewSymbols.setAdapter(mSymbolsAdapter);
         mRecyclerViewSymbols.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false));
 
-        long user_id = mDBHelper.getLastUser();
-        Log.d("User", String.valueOf(user_id));
-
-        String soat = mSP.getString("soat", "");
-        if (soat != null && soat != ""){
-            mDBHelper.createNewDate(getApplicationContext().getString(R.string.doc_soat), soat, "SOAT", user_id);
-        }
-
-        String rtm = mSP.getString("rtm", "");
-        if (rtm != null && soat != ""){
-            mDBHelper.createNewDate(getApplicationContext().getString(R.string.doc_rtm), rtm, "RTM", user_id);
-        }
-
-        String src = mSP.getString("src", "");
-        if (src != null && soat != ""){
-            mDBHelper.createNewDate(getApplicationContext().getString(R.string.doc_src), src, "SRC", user_id);
-        }
-
-        String str = mSP.getString("str", "");
-        if (str != null && soat != ""){
-            mDBHelper.createNewDate(getApplicationContext().getString(R.string.doc_str), str, "STR", user_id);
-        }
-
-        String to = mSP.getString("to", "");
-        if (to != null && soat != ""){
-            mDBHelper.createNewDate(getApplicationContext().getString(R.string.doc_tao), to, "TO", user_id);
-        }
-
-        String ext = mSP.getString("ext", "");
-        if (ext != null && soat != ""){
-            mDBHelper.createNewDate(getApplicationContext().getString(R.string.doc_ext), ext, "EXT", user_id);
-        }
-
         mDetailsAdapter = new HomeDetailsAdapter(mContext, mDBHelper.getAllDates());
 
         mRecyclerViewDetails.setAdapter(mDetailsAdapter);
